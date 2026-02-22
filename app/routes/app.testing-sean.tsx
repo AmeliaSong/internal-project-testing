27ed6429d4789498081b87f041e9180bbf01c5f7
Sean Wang <sean@symmetrycommerce.com>
2026-02-22 16:47:05 -0500

    Blog information basic layout done

--- app/routes/app.testing-sean.tsx
+++ app/routes/app.testing-sean.tsx
@@ -1,26 +1,82 @@
-export default function AdditionalPage() {
+import { useLoaderData } from "react-router";
+
+import type { LoaderFunctionArgs } from "react-router";
+import { authenticate } from "../shopify.server";
+
+export const loader = async ({ request }: LoaderFunctionArgs) => {
+  const { admin } = await authenticate.admin(request);
+
+  const response = await admin.graphql(
+    `#graphql
+      query GetBlogs {
+        blogs(first: 5) {
+          edges {
+            node {
+              id
+              title
+              handle
+              articles(first: 5) {
+                edges {
+                  node {
+                    id
+                    title
+                    handle
+                    body
+                    summary
+                  }
+                }
+              }
+            }
+          }
+        }
+      }
+    `
+  );
+
+  const responseJson = await response.json();
+
+  return {
+    blogs: responseJson.data.blogs.edges,
+  };
+};
+
+export default function TestingPageSean() {
+  const { blogs } = useLoaderData<typeof loader>();
+
   return (
-    <s-page heading="Testing page Sean">
-      <s-section heading="Multiple pages">
-        <s-paragraph>
-          Blog information:
-        </s-paragraph>
-        <s-paragraph>
-          blah <code>yes</code>
-        </s-paragraph>
-      </s-section>
-      <s-section slot="aside" heading="Resources">
-        <s-unordered-list>
-          <s-list-item>
-            <s-link
-              href="https://shopify.dev/docs/apps/design-guidelines/navigation#app-nav"
-              target="_blank"
-            >
-              App nav best practices
-            </s-link>
-          </s-list-item>
-        </s-unordered-list>
-      </s-section>
+    <s-page heading="Blog Information">
+      {blogs.map((blogEdge) => {
+        const blog = blogEdge.node;
+
+        return (
+          <div key={blog.id}>
+            <h2>{blog.title}</h2>
+            <p><strong>Handle:</strong> {blog.handle}</p>
+            <br></br>
+
+            {blog.articles.edges.map((articleEdge) => {
+              const article = articleEdge.node;
+
+              return (
+                <>
+                  <s-section key={article.id} heading={article.title}>
+                    <h3>Summary</h3>
+                    <p>{article.summary}</p>
+                    <h3>Info: </h3>
+                    <p>Images with no alt text: {}</p>
+                    <div
+                      dangerouslySetInnerHTML={{
+                        __html: article.bodyHtml,
+                      }}
+                    />
+                  </s-section>
+                  <br></br>
+                </>
+              );
+            })}
+          </div>
+        );
+      })}
     </s-page>
   );
 }
